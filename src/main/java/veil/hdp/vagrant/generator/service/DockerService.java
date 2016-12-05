752f50173aadcd70620c829ffd1bb6203c412540
Tim Veil <tjveil@gmail.com>
2016-12-04 20:29:34 -0500

    working on docker; not working

--- src/main/java/veil/hdp/vagrant/generator/service/DockerService.java
+++ src/main/java/veil/hdp/vagrant/generator/service/DockerService.java
@@ -16,17 +16,19 @@ public class DockerService extends AbstractFileService {
     @Override
     protected void buildFile(Map<String, Object> model, Arguments arguments) {
 
-        String installShellScript = convertTemplateToString(resolver, "templates/common/shell/shell-install.mustache", model);
-        String cleanLogsShellScript = convertTemplateToString(resolver, "templates/common/shell/shell-clean-logs.mustache", model);
         String dockerFile = convertTemplateToString(resolver, "templates/docker/dockerfile.mustache", model);
+        String krb5 = convertTemplateToString(resolver, "templates/common/kerberos/krb5.mustache", model);
+        String kdc = convertTemplateToString(resolver, "templates/common/kerberos/kdc.mustache", model);
+        String kadm5 = convertTemplateToString(resolver, "templates/common/kerberos/kadm5.mustache", model);
 
 
         final String parentDirectoryName = "out/" + arguments.getFqdn();
 
         try {
             FileUtils.writeStringToFile(new File(parentDirectoryName, DOCKERFILE), dockerFile, StandardCharsets.UTF_8);
-            FileUtils.writeStringToFile(new File(parentDirectoryName, "shell-clean-logs.sh"), cleanLogsShellScript, StandardCharsets.UTF_8);
-            FileUtils.writeStringToFile(new File(parentDirectoryName, "shell-install.sh"), installShellScript, StandardCharsets.UTF_8);
+            FileUtils.writeStringToFile(new File(parentDirectoryName, "krb5.conf"), krb5, StandardCharsets.UTF_8);
+            FileUtils.writeStringToFile(new File(parentDirectoryName, "kdc.conf"), kdc, StandardCharsets.UTF_8);
+            FileUtils.writeStringToFile(new File(parentDirectoryName, "kadm5.acl"), kadm5, StandardCharsets.UTF_8);
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }
